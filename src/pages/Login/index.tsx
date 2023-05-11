07326197fce5a3bc6b375d2294d793593d309b73
Gabrielle-SAVARY <savary.gabrielle@hotmail.fr>
2023-05-11 10:14:51 +0200

    Login page add style

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,23 +1,22 @@
 import { NavLink } from 'react-router-dom';
-import Field from './Field';
 import './styles.scss';
 
 function Login() {
   return (
 
-    <div className="login">
-      <form action="submit" className="login-form">
-        <h3>Me connecter</h3>
-        <div className="login-form__field__container">
-          <Field placeholderLabel="Email" value="" type="text" placeholder="adresse email" />
-          <Field placeholderLabel="Mot de passe" value="" type="text" placeholder="mot de passe" />
-        </div>
-        <button type="submit" className="login-form__button">Connexion</button>
-      </form>
-      <p>Mot de passe oublié</p>
-      <div className="register-redirect">
-        <p>Vous n&apos; avez pas encore de compte</p>
-        <button type="submit" className="button-register">Inscription</button>
+    <div className="login-page">
+      <div className="login-page__wrapper">
+
+        <form action="submit" className="form form-login">
+          <div className="form__logo">Quiz&apos;O&apos;tron</div>
+          <input type="text" placeholder="Pseudo" className="form__input" />
+          <input type="password" placeholder="Mot de passe" className="form__input" />
+          <button type="submit" className="form__button">Connexion</button>
+          <p className="form__message">
+            Pas encore de compte?
+            <NavLink to="/inscription" className="form__inscription">Inscription</NavLink>
+          </p>
+        </form>
       </div>
     </div>
 
