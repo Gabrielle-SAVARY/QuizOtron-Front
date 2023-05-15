0b3375ffb037a187315f01f00e3868ab5e9f16c8
Gabrielle-SAVARY <savary.gabrielle@hotmail.fr>
2023-05-15 16:03:09 +0200

    create button: delete profile

--- src/pages/Profil/index.tsx
+++ src/pages/Profil/index.tsx
@@ -1,8 +1,18 @@
-function Profil() {
-  return (
+import { ChangeEvent, FormEvent } from 'react';
+import { useAppDispatch, useAppSelector } from '../../hooks/redux';
+import './styles.scss';
 
+function Profil() {
+  const dispatch = useAppDispatch();
+  const pseudo = useAppSelector((state) => state.user.credentials.pseudo);
+  return (
     <div className="profil">
-      Profile
+      <p>
+        {`Bienvenue sur ton profile ${pseudo}`}
+      </p>
+      <button type="submit" className="profil__delete">
+        Supprimer mon compte
+      </button>
     </div>
 
   );
