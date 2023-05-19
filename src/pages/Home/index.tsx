975bd11808a9fcf501ba336d808959a4b5140acd
RomainAndine <romainandine@gmail.com>
2023-05-19 10:07:14 +0200

    Add quiz list from BDD

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,15 +1,51 @@
-import Card from '../../components/Card';
+import './styles.scss';
 
 function Home() {
   return (
-
-    <div className="home">
-      <Card />
-
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officiis voluptates in ullam optio numquam? Adipisci, iure explicabo, aliquam sequi ut enim quaerat quos quis accusantium, magnam tempora voluptatibus ad consequatur!</p>
-
+    <div className="home__bg">
+      <div className="home">
+        <h1 className="home__title">
+          Bienvenue sur le site de Quiz&apos;O&apos;Tron!
+        </h1>
+        <p className="home__description">
+          Bienvenue sur notre site de quiz, l&apos;endroit idéal pour mettre à
+          l&apos;épreuve vos connaissances et vous divertir! Que vous soyez un
+          passionné de trivia, un amateur de culture générale ou simplement à la
+          recherche d&apos;une façon amusante de tester vos compétences, notre
+          site de quiz est fait pour vous.
+        </p>
+        <p className="home__description">
+          Plongez dans notre vaste collection de quiz stimulants couvrant une
+          multitude de sujets passionnants. Que ce soit l&apos;histoire, la
+          science, la géographie, le cinéma, la musique, le sport ou bien
+          d&apos;autres domaines, vous trouverez ici des quiz qui éveilleront
+          votre curiosité et vous permettront d&apos;en apprendre davantage.
+        </p>
+        <p className="home__description">
+          Nous proposons des quiz adaptés à tous les niveaux, que vous soyez
+          débutant ou expert. Vous pouvez choisir parmi des quiz à choix
+          multiples, des questions ouvertes, des images à deviner, et bien plus
+          encore. Mettez vos méninges à l&apos;épreuve et défiez vos amis pour
+          voir qui obtiendra le meilleur score.
+        </p>
+        <p className="home__description">
+          Notre site de quiz est conçu pour offrir une expérience conviviale et
+          intuitive. Vous pouvez créer un compte pour suivre votre progression,
+          enregistrer vos scores et accéder à des fonctionnalités exclusives.
+        </p>
+        <p className="home__description">
+          Que vous cherchiez à vous divertir, à apprendre de nouvelles choses ou
+          à relever des défis intellectuels, notre site de quiz est là pour
+          vous. Préparez-vous à tester vos connaissances, à découvrir des faits
+          fascinants et à vous amuser en vous plongeant dans l&apos;univers
+          captivant des quiz.
+        </p>
+        <p className="home__description">
+          Rejoignez-nous dès maintenant et commencez votre aventure passionnante
+          avec les quiz les plus captivants du web!
+        </p>
+      </div>
     </div>
-
   );
 }
 
