75c1f1ed9be43aef6dd9ee805284dbaed6cd9217
Gabrielle-SAVARY <savary.gabrielle@hotmail.fr>
2023-05-19 10:53:04 +0200

    get allTags and selected tag from menu dropdown

--- src/pages/Create-Quiz/index.tsx
+++ src/pages/Create-Quiz/index.tsx
@@ -7,6 +7,8 @@ import { ITag } from '../../@types/tag';
 import './styles.scss';
 import { axiosInstance } from '../../utils/axios';
 import { ILevel } from '../../@types/level';
+import { useAppDispatch, useAppSelector } from '../../hooks/redux';
+import { getTags, setQuizTag, setQuizTagId } from '../../store/reducers/quizCreate';
 
 interface CreateQuizProps {
   quizTitle: string;
@@ -22,87 +24,38 @@ interface CreateQuizProps {
 }
 
 function CreateQuiz() {
-  // ---- STATE ----
-  // State: inputs du formulaire de création
-  const [quiz, setQuiz] = useState<CreateQuizProps[]>([]);
-  // State: liste des tags/catégories possibles pour un quiz
-  const [tags, setTags] = useState<ITag[]>([]);
-  // State: level/niveau sélectionné pour le quiz en cours de création
-  const [quizTag, setQuizTag] = useState('');
-  const [levels, setLevels] = useState<ILevel[]>([]);
-  // State: level/niveau sélectionné pour le quiz en cours de création
-  const [quizLevel, setQuizLevel] = useState('');
-
-  // Met à jour le state quiz avec la valeur des inputs du formulaire
-  const handleChangeField = (event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>): void => {
-    // récupère le name et la valeur de l'input et le type la donnée
-    const fieldName = event.target.name as KeysOfICreateQuiz;
-    const newValue = event.target.value;
-    // reprend les données déjà présentes dans le state quiz
-    // et ajoute ou met à jour les nouvelles données
-    setQuiz((prevQuiz) => ({ ...prevQuiz, [fieldName]: newValue }));
-  };
+  const dispatch = useAppDispatch();
 
   // ---- TAG/CATEGOERIE DU QUIZ ----
-  // Met à jour le state quizTag: selon la catégorie sélectionnée dans le menu déroulant
+  const allTags = useAppSelector((state) => state.quizCreate.allTags);
+  console.log('allTags', allTags);
+  const selectedTagName = useAppSelector((state) => state.quizCreate.selectedTagName);
+
+  // récupère la liste des catégories pour les afficher dans le menu déroulant
+  useEffect(() => {
+    dispatch(getTags());
+  }, [dispatch]);
+
+  // récupère l'id de la catégorie sélectionné dans le menu déroulant
+  // et met à jour le state: tag_id,
+  // se déclenche si le state: selectedTagName est mis à jour
+  useEffect(() => {
+    const findTagId = () => {
+      const foundTag = allTags.find((tag) => tag.name === selectedTagName);
+      if (foundTag) {
+        console.log('foundTag', foundTag.id);
+        dispatch(setQuizTagId(foundTag.id));
+      }
+    };
+    findTagId();
+  }, [allTags, dispatch, selectedTagName]);
+
+  // récupère le nom de la catégorie sélectionnée dans le menu déroulant
   const handleChangeTag = (event: ChangeEvent<HTMLSelectElement>) : void => {
-    setQuizTag(event.target.value);
+    const newTag = event.target.value;
+    console.log('newTag', newTag);
+    dispatch(setQuizTag(newTag));
   };
-
-  // Appel API: récupère la liste des catégories/tags
-  const getTags = async () => {
-    try {
-      const response = await axiosInstance.get('/tag');
-      // Si pas de réponse 200 envoi erreur
-      if (response.status !== 200) {
-        throw new Error();
-      }
-      // met à jour le state avec les données envoyées par l'API
-      setTags(response.data);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  // Récupère la liste des tags si le tag du quiz n'est pas sélectionné dans le menu déroulant
-  useEffect(() => {
-    // si tag/catégorie n'est pas sélectionné, faire un appel APi: récupérer la liste tags
-    if (!quizTag) {
-      getTags();
-    }
-  }, [quizTag]);
-
-  // ---- DIFFICULTY/NIVEAU DE DIFFICULTE DU QUIZ ----
-  // Appel API: récupère la liste des catégories/tags
-
-  // Met à jour le state quizTag: selon la catégorie sélectionnée dans le menu déroulant
-  const handleChangeLevel = (event: ChangeEvent<HTMLSelectElement>) : void => {
-    setQuizLevel(event.target.value);
-  };
-
-  const getLevel = async () => {
-    try {
-      const response = await axiosInstance.get('/level');
-      console.log('response', response);
-      console.log('response DATA', response.data);
-      // Si pas de réponse 200 envoi erreur
-      if (response.status !== 200) {
-        throw new Error();
-      }
-      // met à jour le state avec les données envoyées par l'API
-      setLevels(response.data);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  // Récupère la liste des niveaux si n'est pas sélectionné dans le menu déroulant
-  useEffect(() => {
-    // si le niveau n'est pas sélectionné, faire un appel APi
-    if (!quizLevel) {
-      getLevel();
-    }
-  }, [quizLevel]);
 
   return (
     <form action="submit">
@@ -116,19 +69,14 @@ function CreateQuiz() {
           <select name="Catégorie" id="category-quiz" className="quiz__selector" onChange={handleChangeTag}>
             <option value="">Merci de choisir une catégorie</option>
             {
-                tags.map((tag) => (
+                allTags.map((tag) => (
                   <option key={tag.id} value={tag.name}>{tag.name}</option>
                 ))
             }
           </select>
           <label htmlFor="level-quiz">Choisissez une difficulté</label>
-          <select name="level" id="level-quiz" className="quiz__selector" onChange={handleChangeLevel}>
+          <select name="level" id="level-quiz" className="quiz__selector">
             <option value="">Merci de choisir une difficulté</option>
-            {
-                levels.map((level) => (
-                  <option key={level.id} value={level.name}>{level.name}</option>
-                ))
-            }
           </select>
           <label htmlFor="titre-quiz">Choisissez votre titre de quiz</label>
           <input
@@ -136,12 +84,11 @@ function CreateQuiz() {
             placeholder="Titre du quiz"
             className="quiz__selector"
             name="quizTitle"
-            value={quiz.quizTitle}
-            onChange={handleChangeField}
+
           />
 
           <label htmlFor="description-quiz">Choisissez votre description de quiz</label>
-          <textarea className="quiz__selector" name="quizDescription" id="" cols={30} rows={10} placeholder="Votre description de quiz..." value={quiz.quizDescription} onChange={handleChangeField} />
+          <textarea className="quiz__selector" name="quizDescription" id="" cols={30} rows={10} placeholder="Votre description de quiz..." />
         </div>
         <div className="question__container">
 
@@ -153,20 +100,19 @@ function CreateQuiz() {
               placeholder="Question"
               className="question__title"
               name="quizQuestion"
-              value={quiz.quizQuestion}
-              onChange={handleChangeField}
+
             />
             <fieldset>
               <div className="question-choice">
                 {/* réponse 1 */}
                 <div className="answer_container">
                   <span className="answer_radio-button">
-                    <input type="radio" id="question1-radio1" name="question1-radio" value={quiz.question1Answer} onChange={handleChangeField} />
+                    <input type="radio" id="question1-radio1" name="question1-radio" />
                     <label htmlFor="question1-radio1" />
                   </span>
                   <span className="answer_input-text">
                     <label htmlFor="question1-answer1" />
-                    <input type="text" id="question1-answer1" name="question1Answer1" value={quiz.question1Answer1} onChange={handleChangeField} />
+                    <input type="text" id="question1-answer1" name="question1Answer1" />
                   </span>
                 </div>
                 {/* réponse 2 */}
@@ -177,7 +123,7 @@ function CreateQuiz() {
                   </span>
                   <span className="answer_input-text">
                     <label htmlFor="question1-answer2" />
-                    <input type="text" id="question1-answer2" name="question1Answer2" value={quiz.question1Answer2} onChange={handleChangeField} />
+                    <input type="text" id="question1-answer2" name="question1Answer2" />
                   </span>
                 </div>
                 {/* réponse 3 */}
@@ -188,7 +134,7 @@ function CreateQuiz() {
                   </span>
                   <span className="answer_input-text">
                     <label htmlFor="question1-answer3" />
-                    <input type="text" id="question1-answer3" name="question1Answer3" value={quiz.question1Answer3} onChange={handleChangeField} />
+                    <input type="text" id="question1-answer3" name="question1Answer3" />
                   </span>
                 </div>
                 {/* réponse 4 */}
@@ -199,7 +145,7 @@ function CreateQuiz() {
                   </span>
                   <span className="answer_input-text">
                     <label htmlFor="question1-answer4" />
-                    <input type="text" id="question1-answer4" name="question1Answer4" value={quiz.question1Answer4} onChange={handleChangeField} />
+                    <input type="text" id="question1-answer4" name="question1Answer4" />
                   </span>
                 </div>
               </div>
--- src/store/reducers/quizCreate.ts
+++ src/store/reducers/quizCreate.ts
@@ -37,9 +37,36 @@ export const initialState: QuizCreateState = {
   allLevels: [],
   selectedLevelName: '',
 };
+// ---- TAG/CATEGOERIE DU QUIZ ----
+// Appel API: récupère la liste des catégories/tags
+export const getTags = createAppAsyncThunk(
+  'createQuiz/GET_ALL_TAGS',
+  async () => {
+    const { data } = await axiosInstance.get('/tag');
+    console.log('DATA TAGS', data);
 
-const quizCreateReducer = createReducer(initialState, () => {
+    return data as Tag[];
+  },
+);
 
+// Action: récupère la catégorie sélectionné par le menu déroulant
+export const setQuizTag = createAction<string>('createQuiz/SET_QUIZ_TAG');
+
+// Action: récupère l'id de la catégorie sélectionné par le menu déroulant
+export const setQuizTagId = createAction<number>('createQuiz/SET_QUIZ_TAG_ID');
+
+const quizCreateReducer = createReducer(initialState, (builder) => {
+  builder
+    // CATEGORIE
+    .addCase(getTags.fulfilled, (state, action) => {
+      state.allTags = action.payload;
+    })
+    .addCase(setQuizTag, (state, action) => {
+      state.selectedTagName = action.payload;
+    })
+    .addCase(setQuizTagId, (state, action) => {
+      state.quiz.tag_id = action.payload;
+    });
 });
 
 export default quizCreateReducer;
