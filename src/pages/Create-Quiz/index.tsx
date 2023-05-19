1498121a1d77e92ef1d638183d8bad1d7692574e
Gabrielle-SAVARY <savary.gabrielle@hotmail.fr>
2023-05-19 10:14:28 +0200

    get input and dropdown from form with local states

--- src/pages/Create-Quiz/index.tsx
+++ src/pages/Create-Quiz/index.tsx
@@ -1,9 +1,12 @@
-import { useState, ChangeEvent, FormEvent } from 'react';
+import {
+  useState, ChangeEvent, FormEvent, useEffect,
+} from 'react';
 import { ICreateQuiz, KeysOfICreateQuiz } from '../../@types/quiz';
 import { ITag } from '../../@types/tag';
 
 import './styles.scss';
 import { axiosInstance } from '../../utils/axios';
+import { ILevel } from '../../@types/level';
 
 interface CreateQuizProps {
   quizTitle: string;
@@ -14,45 +17,92 @@ interface CreateQuizProps {
   question1Answer3: string;
   question1Answer4: string;
   question1Answer: string;
+  quizTag: string;
+  quizLevel: string;
 }
 
 function CreateQuiz() {
-  const [tag, setTag] = useState<[]>();
+  // ---- STATE ----
+  // State: inputs du formulaire de création
+  const [quiz, setQuiz] = useState<CreateQuizProps[]>([]);
+  // State: liste des tags/catégories possibles pour un quiz
+  const [tags, setTags] = useState<ITag[]>([]);
+  // State: level/niveau sélectionné pour le quiz en cours de création
+  const [quizTag, setQuizTag] = useState('');
+  const [levels, setLevels] = useState<ILevel[]>([]);
+  // State: level/niveau sélectionné pour le quiz en cours de création
+  const [quizLevel, setQuizLevel] = useState('');
 
-  const [quiz, setQuiz] = useState<CreateQuizProps>({
-    quizTitle: '',
-    quizDescription: '',
-    quizQuestion: '',
-    question1Answer1: '',
-    question1Answer2: '',
-    question1Answer3: '',
-    question1Answer4: '',
-    question1Answer: '',
-  });
-
-  // Met à jour le state avec la valur des inputs du formulaire
+  // Met à jour le state quiz avec la valeur des inputs du formulaire
   const handleChangeField = (event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>): void => {
     // récupère le name et la valeur de l'input et le type la donnée
     const fieldName = event.target.name as KeysOfICreateQuiz;
     const newValue = event.target.value;
-    console.log('fieldName', fieldName);
-    console.log('newValue', newValue);
+    // reprend les données déjà présentes dans le state quiz
+    // et ajoute ou met à jour les nouvelles données
     setQuiz((prevQuiz) => ({ ...prevQuiz, [fieldName]: newValue }));
   };
 
-  // récupère la liste des catégories de quiz
+  // ---- TAG/CATEGOERIE DU QUIZ ----
+  // Met à jour le state quizTag: selon la catégorie sélectionnée dans le menu déroulant
+  const handleChangeTag = (event: ChangeEvent<HTMLSelectElement>) : void => {
+    setQuizTag(event.target.value);
+  };
+
+  // Appel API: récupère la liste des catégories/tags
   const getTags = async () => {
     try {
       const response = await axiosInstance.get('/tag');
-      console.log('response', response);
+      // Si pas de réponse 200 envoi erreur
       if (response.status !== 200) {
         throw new Error();
       }
-      setTag(response.data);
+      // met à jour le state avec les données envoyées par l'API
+      setTags(response.data);
     } catch (error) {
       console.log(error);
     }
   };
+
+  // Récupère la liste des tags si le tag du quiz n'est pas sélectionné dans le menu déroulant
+  useEffect(() => {
+    // si tag/catégorie n'est pas sélectionné, faire un appel APi: récupérer la liste tags
+    if (!quizTag) {
+      getTags();
+    }
+  }, [quizTag]);
+
+  // ---- DIFFICULTY/NIVEAU DE DIFFICULTE DU QUIZ ----
+  // Appel API: récupère la liste des catégories/tags
+
+  // Met à jour le state quizTag: selon la catégorie sélectionnée dans le menu déroulant
+  const handleChangeLevel = (event: ChangeEvent<HTMLSelectElement>) : void => {
+    setQuizLevel(event.target.value);
+  };
+
+  const getLevel = async () => {
+    try {
+      const response = await axiosInstance.get('/level');
+      console.log('response', response);
+      console.log('response DATA', response.data);
+      // Si pas de réponse 200 envoi erreur
+      if (response.status !== 200) {
+        throw new Error();
+      }
+      // met à jour le state avec les données envoyées par l'API
+      setLevels(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  // Récupère la liste des niveaux si n'est pas sélectionné dans le menu déroulant
+  useEffect(() => {
+    // si le niveau n'est pas sélectionné, faire un appel APi
+    if (!quizLevel) {
+      getLevel();
+    }
+  }, [quizLevel]);
 
   return (
     <form action="submit">
@@ -63,18 +113,22 @@ function CreateQuiz() {
         </div>
         <div className="quiz__parameter">
           <label htmlFor="category-quiz">Choisissez une catégorie</label>
-          <select name="Catégorie" id="category-quiz" className="quiz__selector">
+          <select name="Catégorie" id="category-quiz" className="quiz__selector" onChange={handleChangeTag}>
             <option value="">Merci de choisir une catégorie</option>
-            <option value="Catégorie 1">Catégorie 1</option>
-            <option value="Catégorie 2">Catégorie 2</option>
-            <option value="Catégorie 2">Catégorie 3</option>
+            {
+                tags.map((tag) => (
+                  <option key={tag.id} value={tag.name}>{tag.name}</option>
+                ))
+            }
           </select>
           <label htmlFor="level-quiz">Choisissez une difficulté</label>
-          <select name="level" id="level-quiz" className="quiz__selector">
+          <select name="level" id="level-quiz" className="quiz__selector" onChange={handleChangeLevel}>
             <option value="">Merci de choisir une difficulté</option>
-            <option value="Difficulté 1">Difficulté 1</option>
-            <option value="Difficulté 2">Difficulté 2</option>
-            <option value="Difficulté 2">Difficulté 3</option>
+            {
+                levels.map((level) => (
+                  <option key={level.id} value={level.name}>{level.name}</option>
+                ))
+            }
           </select>
           <label htmlFor="titre-quiz">Choisissez votre titre de quiz</label>
           <input
