a378a6f5f11f4acf137e2f68f8266fd8861bbcfe
Gabrielle-SAVARY <savary.gabrielle@hotmail.fr>
2023-05-22 14:53:31 +0200

    delete quiz reducer

--- src/store/reducers/index.tsx
+++ src/store/reducers/index.tsx
@@ -1,11 +1,9 @@
 import headerReducer from './header';
-import quizReducer from './quiz';
 import userReducer from './user';
 
 const reducer = {
   user: userReducer,
   header: headerReducer,
-  quiz: quizReducer,
 };
 
 export default reducer;
