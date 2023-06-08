0eb71f148cc245b37aadf85a9434f3a0638a6aab
RomainAndine <romainandine@gmail.com>
2023-06-08 10:48:20 +0200

    Search bar: Fix search filter when word is deleted and empty search

--- src/pages/QuizList/SearchBar/index.tsx
+++ src/pages/QuizList/SearchBar/index.tsx
@@ -4,30 +4,53 @@ import './styles.scss';
 
 interface SearchBarProps {
   quizList: IQuizList[]
-  setQuizFilter: (quiz:IQuizList[]) => void
+  setQuizFilter: (quiz: IQuizList[]) => void
 
 }
 
 function SearchBar({ quizList, setQuizFilter }: SearchBarProps) {
+  //* Stocke la valeur de l'input = requête utilisateur
   const [query, setQuery] = useState<string[]>([]);
+
+  //* Récupère la valeur de l'input et la stock dans le state
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const inputText = event.target.value.split(' ');
-    setQuery(inputText);
+    // Si l'input est vidé par l'utilisateur (state à ['']), on remet le state à tableau vide
+    if (inputText.length === 1 && inputText[0] === '') {
+      setQuery([]);
+    } else {
+      setQuery(inputText);
+    }
   };
 
-  const search = (data:IQuizList[]) => data.filter(
-    (item) => query.some(
-      (keyword) => item.title.toLowerCase().includes(keyword.toLowerCase())
-    || item.description.toLowerCase().includes(keyword.toLowerCase())
-    || item.level.name.toLowerCase().includes(keyword.toLowerCase())
-    || item.tags.some((tag) => tag.name.toLowerCase().includes(keyword.toLowerCase()))
-    || item.author.pseudo.toLowerCase().includes(keyword.toLowerCase()),
-    ),
-  );
+  //* Filtre les quiz en fonction de la recherche
+  //* Permet d'ignorer un mot clé si il est supprimé de la recherche (string vide)
+  // Pour chaque quiz, on supprime les espaces pour chaque keyword du tableau query
+  // Si le mot clé n'est pas vide : on fait la recherche
+  // On vérifie pour chaque critère du quiz si le mot clé est présent
+  // Si oui (renvoie true): on retourne le quiz
+  // Si le mot clé est vide: on ne fait pas de recherche pour ce mot clé
+  const search = (quizArray: IQuizList[], userQuery: string[]) => {
+    if (userQuery.length === 0) {
+      return quizArray;
+    }
+    return quizArray.filter(
+      (quiz) => userQuery.some(
+        (keyword) => keyword.trim() !== '' && (
+          quiz.title.toLowerCase().includes(keyword.toLowerCase())
+          || quiz.description.toLowerCase().includes(keyword.toLowerCase())
+          || quiz.level.name.toLowerCase().includes(keyword.toLowerCase())
+          || quiz.tags.some((tag) => tag.name.toLowerCase().includes(keyword.toLowerCase()))
+          || quiz.author.pseudo.toLowerCase().includes(keyword.toLowerCase())
+        ),
+      ),
+    );
+  };
 
+  //* Soumet la recherche et met à jour le state des quiz filtrés
   const handleSubmitSearch = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    setQuizFilter(search(quizList));
+    setQuizFilter(search(quizList, query));
   };
 
   return (
