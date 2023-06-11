feb12635b360e796f5f5c1bb595d401bc89abfc1
Gabrielle-SAVARY <savary.gabrielle@hotmail.fr>
2023-06-11 18:34:10 +0200

    Profil: fix delete account button overflow on top of the footer

--- src/pages/Profil/index.tsx
+++ src/pages/Profil/index.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useNavigate } from 'react-router-dom';
 import {
   Button,
   Dialog,
@@ -20,6 +20,7 @@ function Profil() {
   const pseudo = useAppSelector((state) => state.user.credentials.pseudo);
   // Ouvre et ferme la modale pour la confirmation de suppression d'un compte utilisateur
   const [showModalAccount, setShowModalAccount] = useState<boolean>(false);
+  const navigate = useNavigate();
 
   //* Ouvre la modale de confirmation pour la suppression d'un compte utilisateur
   const handleOpenModalAccount = () => {
@@ -38,6 +39,7 @@ function Profil() {
   //*  Déconnexion utilisateur
   const handleLogout = () => {
     dispatch(logout());
+    navigate('/connexion');
   };
 
   return (
@@ -58,43 +60,39 @@ function Profil() {
           Modifier mon compte
         </NavLink>
         <button type="button" className="profil__button profil__button__logout" onClick={handleLogout}>
-          <NavLink to="/connexion">
-            Déconnexion
-          </NavLink>
+          Déconnexion
         </button>
 
-        <div>
-          <button
-            type="button"
-            className="profil__button profil__button__delete"
-            onClick={handleOpenModalAccount}
-          >
-            Supprimer mon compte
-          </button>
-          <Dialog
-            open={showModalAccount}
-            onClose={handleCloseModalAccount}
-            aria-describedby="alert-dialog-slide-description"
-          >
-            <DialogTitle>Voulez-vous vraiment supprimer votre compte ?</DialogTitle>
-            <DialogContent>
-              <DialogContentText id="alert-dialog-slide-description2">
-                Attention cette action est irréversible
-              </DialogContentText>
-            </DialogContent>
-            <DialogActions>
-              <Button onClick={handleCloseModalAccount} variant="contained">Annuler</Button>
-              <Button
-                endIcon={<DeleteIcon />}
-                onClick={handleDeleteUser}
-                variant="contained"
-                color="error"
-              >
-                Supprimer
-              </Button>
-            </DialogActions>
-          </Dialog>
-        </div>
+        <button
+          type="button"
+          className="profil__button profil__button__delete"
+          onClick={handleOpenModalAccount}
+        >
+          Supprimer mon compte
+        </button>
+        <Dialog
+          open={showModalAccount}
+          onClose={handleCloseModalAccount}
+          aria-describedby="alert-dialog-slide-description"
+        >
+          <DialogTitle>Voulez-vous vraiment supprimer votre compte ?</DialogTitle>
+          <DialogContent>
+            <DialogContentText id="alert-dialog-slide-description2">
+              Attention cette action est irréversible
+            </DialogContentText>
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={handleCloseModalAccount} variant="contained">Annuler</Button>
+            <Button
+              endIcon={<DeleteIcon />}
+              onClick={handleDeleteUser}
+              variant="contained"
+              color="error"
+            >
+              Supprimer
+            </Button>
+          </DialogActions>
+        </Dialog>
 
       </div>
 
