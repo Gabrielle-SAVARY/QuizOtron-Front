fa40e20c062ff87cbd807e00d63947701d4e5f6d
Gabrielle-SAVARY <savary.gabrielle@hotmail.fr>
2023-07-19 12:12:58 +0200

    Update: Readme file with installation steps + update css ProfileFavorites

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Quiz-O-Tron-Front
+## Instructions d'installation
+
+1. Clôner le repo Front
+2. Installer le repo `npm i` 
+3. Lancer le repo Back avec `nodemon`
+4. Lancer le front avecc `yarn dev`
+5. Ouvrir un navigateur et aller sur l'adresse `http://localhost:5173/`
--- src/pages/ProfilFavorites/index.tsx
+++ src/pages/ProfilFavorites/index.tsx
@@ -12,31 +12,30 @@ function ProfilFavorites({
   userFavoritesQuiz, addQuizToFavorite, deleteQuizToFavorite,
 }: ProfilQuizProps) {
   return (
-    <div className="quiz__favoris">
-      <h1 className="quiz__title">Mes quiz favoris</h1>
-      <BtnExit/>
+    <div className="quiz-favoris">
+      <div className="quiz-favoris__header">
+        <BtnExit/>
+        <h1 className="quiz-favoris__header-title">Mes quiz favoris</h1>
+      </div>
       <div>
         {userFavoritesQuiz && (
-
-        <div className="quiz__content-list">
-          {userFavoritesQuiz.map((quiz) => (
-            <Card
-              key={quiz.id}
-              id={quiz.id}
-              title={quiz.title}
-              thumbnail={quiz.thumbnail}
-              author={quiz.author.pseudo}
-              level={quiz.level.name}
-              tags={quiz.tags}
-              addQuizToFavorite={addQuizToFavorite}
-              userFavoritesQuiz={userFavoritesQuiz}
-              deleteQuizToFavorite={deleteQuizToFavorite}
-            />
-          ))}
-        </div>
-
+          <div className="quiz-favoris__content-list">
+            {userFavoritesQuiz.map((quiz) => (
+              <Card
+                key={quiz.id}
+                id={quiz.id}
+                title={quiz.title}
+                thumbnail={quiz.thumbnail}
+                author={quiz.author.pseudo}
+                level={quiz.level.name}
+                tags={quiz.tags}
+                addQuizToFavorite={addQuizToFavorite}
+                userFavoritesQuiz={userFavoritesQuiz}
+                deleteQuizToFavorite={deleteQuizToFavorite}
+              />
+            ))}
+          </div>
         )}
-
       </div>
     </div>
   );
